78d6aa7e0ada8ef541d2ced299996fbd6d80fd3a
rishabhdasu <rishabhdasu@gmail.com>
2021-05-26 01:56:22 +0530

    Add

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -1,18 +1,34 @@
 // 1. Define a variable named `students` and assign a blank object to it.
 
+let students = {}
+
 // 2. Add a key of the value of variable `studentName` in the object created above with the value of your name.
+
+students.studentName = "Rishabh";
 
 // 3. Using `console.log` log the value of `studentName` key from the `students` object
 
+console.log(students.studentName);
+
 // 4. Add a another key of the value of variable `batch` in the object with the value of 16.
+
+students.batch = 16;
 
 // 5. Using `console.log` log the value of `batch` key from the `students` object
 
+console.log(students.batch);
+
 // 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
+
+students.isAdult = true;
 
 // 7. Using `console.log` print the value of `isAdult` key from the `students` object
 
+console.log(students.isAdult);
+
 // 8. Add a another key of `42` to the object with a value of `The answer to the meaning of life`.
+
+students["42"] = "The answer to the meaning of life";
 
 // 9. Using `console.log` print the value of key `42` from the `students` object
 
