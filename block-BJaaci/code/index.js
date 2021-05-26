400b286f198df4b045c63bc1a52d1a4786aeb3bd
rishabhdasu <rishabhdasu@gmail.com>
2021-05-26 17:15:55 +0530

    Add

--- block-BJaaci/code/index.js
+++ block-BJaaci/code/index.js
@@ -32,43 +32,99 @@ Use the variable named movies to solve the requirement below.
 
 // 1. Log the value of the title key in the first movie of the array named `movies`
 
+console.log(movies[0].title);
+
 // 2. Log the value of the title key in the last movie of the array named `movies`
+
+console.log(movies[movies.length -1]. title);
 
 // 3. Log the value of the title key in the second movie (index 1) of the array named `movies`
 
+console.log(movies[1].title);
+
 // 4. Log the value of the year key in the first movie of the array named `movies`
+
+console.log(movies[0].year);
 
 // 5. Log all the named of actors (key named actors) of the first movie
 
+console.log(movies[0].actors);
+
+
 // 6. Log the name of the first actor of the first movie
+
+console.log(movies[0].actors[0]);
+
 
 // 7. Log the name of the last actor of the first movie
 
+console.log(movies[0].actors[movies[0].actors.length -1]);
+
 // 8. Log the name of the second actor (index 1) of the first movie
+
+console.log(movies[0].actors[1]);
+
 
 // 9. Log the name of the second actor (index 1) of the second movie
 
+console.log(movies[1].actors[1]);
+
+
 // 10. Log the name of the last actor of the thrid movie
+
+console.log(movies[2].actors[movies[2].actors.length -1]);
+
 
 // 11. Log the name of the second actor (index 1) of the third movie
 
+console.log(movies[2].actors[1]);
+
+
 // 12. Log all the genres of the third movie
+
+console.log(movies[2].genres);
+
 
 // 13. Log all the genres of the first movie
 
+console.log(movies[0].genres);
+
+
 // 14. Log the first genres of the first movie
+
+console.log(movies[0].genres[0]);
+
 
 // 15. Log the first genres of the second movie
 
+console.log(movies[1].genres[0]);
+
+
 // 16. Log the last genres of the first movie (using the length property of array)
+
+console.log(movies[0].genres[movies[0].genres.length - 1]);
+
 
 // 17. Log the first genres of the second movie (using the length property of array)
 
+console.log(movies[1].genres[0]);
+
+
 // 18. Log all the genres of the first movie one by one
+
+for(let genre of movies[0].genres){
+  console.log(genre);
+}
 
 // 19. Log all the genres of the second movie one by one
 
+for(let genre of movies[1].genres){
+  console.log(genre);
+}
+
 // 20. Log if the first actor of the first movie is `Rachel McAdams` or not (You have to log true or false)
+
+console.log(movies[0].actors[0] === `Rachel McAdams` ? true:false);
 
 // 21. Log if the second actor (index 1) of the second movie is `Natalie Portman` or not (You have to log true or false)
 
